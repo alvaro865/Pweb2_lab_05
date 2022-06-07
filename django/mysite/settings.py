93bd274259e59b7c55ae468879eae421de434b16
alvaro865 <asuasaca@unsa.edu.pe>
2022-06-06 20:06:01 -0500

    importando os, agregando nombre del host

--- django/mysite/settings.py
+++ django/mysite/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-_8aqd^8jv-9pis^s%p1!+0zf%-9gai(mbncv&l4--@hjjxkvkn
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
